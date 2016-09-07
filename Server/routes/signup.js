90ae399584d280537ce3d8192237d025027b2ab0
Daniel <danielklein45@gmail.com>
2016-09-07 22:22:51 +0300

    updating

--- Server/routes/signup.js
+++ Server/routes/signup.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var UM = require('DB/Users');
+//var UM = require('DB/Users');
 
 router.get('/', function (req,res) {
     res.render('signup');
